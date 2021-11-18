5ecfe0dbec2fad35995213b09b340d91146088ba
goertler <pascalegoertler@Pascales-MacBook-Air.local>
2021-11-18 15:09:23 -0800

    final csv assignment files, with three additional conisistancy reviews added each

--- scripts/titles_dup.R
+++ scripts/titles_dup.R
@@ -114,3 +114,46 @@ subsample_PG <- assignments_PG[sample(nrow(assignments_PG), 3), ]
 pg <- read.csv("data_clean/PG_assignments_split.csv")
 pg_final <- rbind(pg, subsample_PG)
 write.csv(pg_final, "data_clean/PG_assignments_split.csv")
+
+# not the most efficient, but I dont want to delay abstract reviews...
+assignments_CP <- subset(assignments, consistency_check == "CP")
+subsample_CP <- assignments_CP[sample(nrow(assignments_CP), 3), ]
+
+cp <- read.csv("data_clean/CP_assignments_split.csv")
+cp_final <- rbind(cp, subsample_CP)
+write.csv(cp_final, "data_clean/CP_assignments_split.csv")
+
+assignments_DC <- subset(assignments, consistency_check == "DC")
+subsample_DC <- assignments_DC[sample(nrow(assignments_DC), 3), ]
+
+dc <- read.csv("data_clean/DC_assignments_split.csv")
+dc_final <- rbind(dc, subsample_DC)
+write.csv(dc_final, "data_clean/DC_assignments_split.csv")
+
+assignments_ES <- subset(assignments, consistency_check == "ES")
+subsample_ES <- assignments_ES[sample(nrow(assignments_ES), 3), ]
+
+es <- read.csv("data_clean/ES_assignments_split.csv")
+es_final <- rbind(es, subsample_ES)
+write.csv(es_final, "data_clean/ES_assignments_split.csv")
+
+assignments_LY <- subset(assignments, consistency_check == "LY")
+subsample_LY <- assignments_LY[sample(nrow(assignments_LY), 3), ]
+
+ly <- read.csv("data_clean/LY_assignments_split.csv")
+ly_final <- rbind(ly, subsample_LY)
+write.csv(ly_final, "data_clean/LY_assignments_split.csv")
+
+assignments_MB <- subset(assignments, consistency_check == "MB")
+subsample_MB <- assignments_MB[sample(nrow(assignments_MB), 3), ]
+
+mb <- read.csv("data_clean/MB_assignments_split.csv")
+mb_final <- rbind(mb, subsample_MB)
+write.csv(mb_final, "data_clean/MB_assignments_split.csv")
+
+assignments_RP <- subset(assignments, consistency_check == "RP")
+subsample_RP <- assignments_RP[sample(nrow(assignments_RP), 3), ]
+
+rp <- read.csv("data_clean/RP_assignments_split.csv")
+rp_final <- rbind(rp, subsample_RP)
+write.csv(rp_final, "data_clean/RP_assignments_split.csv")
