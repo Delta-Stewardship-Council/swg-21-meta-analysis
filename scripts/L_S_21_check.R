b37d2e5b945f65cb1f31acaf7c4a32e5665ada15
goertler <pascalegoertler@Pascales-MacBook-Air.local>
2021-12-13 14:51:50 -0800

    checking if we have titles in common with Shen and Liu 2021 (only 5)

---
+++ scripts/L_S_21_check.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(stringr)
+
+SL_21 <- read.csv("data_raw/Shen_Liu_2021 .csv")
+head(SL_21)
+
+SL_titles <- SL_21 %>%
+  # make title lower case
+  mutate(Title = tolower(Title),
+         # strip white spaces at end/beginnings all columns
+         across(.cols=everything(), ~str_trim(.)))
+
+head(SL_titles)
+
+ours <- read.csv("data_clean/assignments_all.csv")
+head(ours)
+
+library(data.table)
+df1 <- as.vector(SL_titles$Title)
+df2 <- as.vector(ours$title)
+setDT(df1, df2)
+
+in_common <- intersect(SL_titles$Title, ours$title)
