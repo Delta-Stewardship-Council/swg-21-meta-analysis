910b41d4b7ada17bae6e8b6b88b33733458a013b
goertler <pascalegoertler@Pascales-MacBook-Air.local>
2022-01-10 16:47:54 -0800

    split assignments

--- scripts/productivity_search_comparison.R
+++ scripts/productivity_search_comparison.R
@@ -137,3 +137,63 @@ head(assignments_pro)
 
 # save out all:
 write.csv(assignments_pro, file = "data_clean/assignments_productivity_search_all.csv", row.names = FALSE)
+
+
+# split and write per person:
+library(purrr)
+library(glue)
+
+assignments_pro %>%
+  group_split(reviewer_name) %>% # split by reviewer initials
+  walk(., ~write_csv(.x, file=glue("data_clean/{.x$reviewer_name[1]}_assignments_productivity_search_split.csv")))
+
+# pull just three for each person (to double check 25%)
+assignments_PG <- subset(assignments_pro, consistency_check == "PG")
+subsample_PG <- assignments_PG[sample(nrow(assignments_PG), 3), ]
+
+pg <- read.csv("data_clean/PG_assignments_productivity_search_split.csv")
+pg_final <- rbind(pg, subsample_PG)
+write.csv(pg_final, "data_clean/PG_assignments_productivity_search_split.csv")
+
+# not the most efficient, but I dont want to delay abstract reviews...
+assignments_CP <- subset(assignments_pro, consistency_check == "CP")
+subsample_CP <- assignments_CP[sample(nrow(assignments_CP), 3), ]
+
+cp <- read.csv("data_clean/CP_assignments_productivity_search_split.csv")
+cp_final <- rbind(cp, subsample_CP)
+write.csv(cp_final, "data_clean/CP_assignments_productivity_search_split.csv")
+
+assignments_DC <- subset(assignments_pro, consistency_check == "DC")
+subsample_DC <- assignments_DC[sample(nrow(assignments_DC), 3), ]
+
+dc <- read.csv("data_clean/DC_assignments_productivity_search_split.csv")
+dc_final <- rbind(dc, subsample_DC)
+write.csv(dc_final, "data_clean/DC_assignments_productivity_search_split.csv")
+
+assignments_ES <- subset(assignments_pro, consistency_check == "ES")
+subsample_ES <- assignments_ES[sample(nrow(assignments_ES), 3), ]
+
+es <- read.csv("data_clean/ES_assignments_productivity_search_split.csv")
+es_final <- rbind(es, subsample_ES)
+write.csv(es_final, "data_clean/ES_assignments_productivity_search_split.csv")
+
+assignments_LY <- subset(assignments_pro, consistency_check == "LY")
+subsample_LY <- assignments_LY[sample(nrow(assignments_LY), 2), ]
+
+ly <- read.csv("data_clean/LY_assignments_productivity_search_split.csv")
+ly_final <- rbind(ly, subsample_LY)
+write.csv(ly_final, "data_clean/LY_assignments_productivity_search_split.csv")
+
+assignments_MB <- subset(assignments_pro, consistency_check == "MB")
+subsample_MB <- assignments_MB[sample(nrow(assignments_MB), 3), ]
+
+mb <- read.csv("data_clean/MB_assignments_productivity_search_split.csv")
+mb_final <- rbind(mb, subsample_MB)
+write.csv(mb_final, "data_clean/MB_assignments_productivity_search_split.csv")
+
+assignments_RP <- subset(assignments_pro, consistency_check == "RP")
+subsample_RP <- assignments_RP[sample(nrow(assignments_RP), 2), ]
+
+rp <- read.csv("data_clean/RP_assignments_productivity_search_split.csv")
+rp_final <- rbind(rp, subsample_RP)
+write.csv(rp_final, "data_clean/RP_assignments_productivity_search_split.csv")
