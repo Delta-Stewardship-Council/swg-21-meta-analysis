f0acbb6cdcd5631652619fe6137e320f5cf5b72d
Ryan Peek <rapeek@ucdavis.edu>
2022-11-03 17:08:35 -0700

    Update w localities

---
+++ scripts/make_map_of_sites.R
@@ -0,0 +1,13 @@
+# map locations
+
+#load packages
+library(tidyverse)
+library(sf)
+library(scales)
+library(tmap)
+library(tmaptools)
+library(rworldmap)
+library(stringr)
+
+#load data
+dat <- read_csv("data_clean/analysis_dat_updated.csv")
